74b84803bb10f5bf2248fa5984faa3e7a7e0098d
Raj <rthummar308@rku.ac.in>
2025-07-21 23:09:58 +0530

    set

--- DSA_js/set.js
+++ DSA_js/set.js
@@ -1,4 +1,24 @@
 //set doesn;t allow duplicate values its remove automatically
-let data= new set(
-     []
-)
+let data = new Set(   //in set S is 'S' not like set but its Set okhy 
+     ['raj','raj','ram','krishna']
+);
+
+let item= ['raj','raj','ram','krishna']
+
+console.log(data.has('ram')); //to check its present  or not
+data.add({'age' : 20});  //its allow to enter object values
+console.log(data);
+console.log(item);
+/*
+//for loop
+for (let x of data ){
+    console.log(x);
+}
+    */
+
+
+//for each loop
+
+data.forEach((val)=>{
+    console.log(val);
+})
