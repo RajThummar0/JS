ec689d75bfaf1f9b65495d5729e3a4f9747292b2
Raj <rthummar308@rku.ac.in>
2025-07-22 00:17:10 +0530

    linked listt

---
+++ DSA_js/linked_list.js
@@ -0,0 +1,28 @@
+class List{
+    constructor(data){
+        this.head={
+            value: data,
+            next: null,
+        };
+        this.tail=this.head;
+        this.size=1;
+    }
+    appendNode(nodedata){
+        let newnode={
+            value:nodedata,
+            next:null
+        };
+        this.tail.next=newnode,
+        this.tail=newnode;
+        this.size+=1;
+    }
+
+}
+
+let list=new List(200);
+list.appendNode(300);
+list.appendNode(400);
+list.appendNode(500);
+list.appendNode(600);
+
+console.log(list);
