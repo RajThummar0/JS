7c1902fee3334e18a828cd52d5f94e05712963d0
Raj <rthummar308@rku.ac.in>
2025-07-29 15:44:37 +0530

    pattern

--- sharpner/pattern1.js
+++ sharpner/pattern1.js
@@ -1,3 +1,19 @@
+/*                      link: https://www.youtube.com/watch?v=aXHanyXQqI0
+        Y = MX+C    or j len = gap * i +    c 
+        Y = here like starting from where actuall there its 25
+        M = what is gap
+        X = from where outer loop will be start like here 10
+        you will find C
+        affter that jlen=gap*i+c
+        c we will know + gap +C
+
+*/
+/*
+
+
+
+
+
 
 function pattern(n){
 for(let i=1;i<=n;i++){
@@ -15,3 +31,13 @@ pattern(8)
 ****
 *****
 */
+
+for(let i=10;i<=13;i++){//
+    for(let j=25;j<=i+15;j++){
+         process.stdout.write(String('*'));
+    }
+    console.log();
+}
+
+
+    
---
+++ sharpner/pattern3.js
@@ -0,0 +1,8 @@
+for(let i=10;i<=13;i++){
+    for(let j=15;j<=-3*i+51;j++){
+        process.stdout.write(String('*'));
+    }
+    console.log();
+}
+
+
